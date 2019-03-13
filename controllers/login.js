b0f66758fa049715c4772878108152366c607100
Dhara <dharababariya34@gmail.com>
2019-03-13 19:21:49 +0530

    create login

--- controllers/login.js
+++ controllers/login.js
@@ -5,7 +5,23 @@ const knex = require('../helpers/knex');
 module.exports = function (app) {
 
 
-    
+    app.get('/', (req, res) => {	
+
+
+        res.render('login');	
+    })	
+
+    app.post('/signin', async function(req,res) {	
+
+        console.log(JSON.stringify(req.body))	
+       const result = knex("library.signin").insert({	
+           email:req.body.email,	
+           password:req.body.password	
+
+        })	
+        res.send(result);
+        console.log(`result`);
+   } )
     
 }
 
