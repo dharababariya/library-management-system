7be52ee3801cd52f3f79321ae0742a7fa84f1431
Dhara <dharababariya34@gmail.com>
2019-03-14 10:13:14 +0530

    login api done

--- controllers/login.js
+++ controllers/login.js
@@ -2,26 +2,37 @@
 
 const knex = require('../helpers/knex');
 
+//add module
+
 module.exports = function (app) {
 
+    app.get('/login', (req, res) => {
 
-    app.get('/', (req, res) => {	
+        res.render('login');
+    });
+    //create login api
+    app.use(function (req, res, next) {
+        if ("OPTION" === req.method) {
 
+            res.sendStatus(200)
+        } else {
+            next();
+        }
+    });
 
-        res.render('login');	
-    })	
+    app.post('/signin', async function (req, res) {
 
-    app.post('/signin', async function(req,res) {	
+        //console.log(JSON.stringify(req.body))
+        try {
 
-        console.log(JSON.stringify(req.body))	
-       const result = knex("library.signin").insert({	
-           email:req.body.email,	
-           password:req.body.password	
+            //insert value in body
+            const result = await knex("library.signin").insert({email: req.body.email, password: req.body.password})
+            console.log(`resule ${JSON.stringify(result)} `)
+        } catch (error) {
+            console.log(error);
 
-        })	
-        res.send(result);
-        console.log(`result`);
-   } )
-    
+        }
+
+    })
+
 }
-
--- controllers/sign_up.js
+++ controllers/sign_up.js
@@ -1,16 +1,16 @@
 //knex config
 
 const knex = require('../helpers/knex');
-
+// add module
 module.exports = function (app) {
-
-    app.get('/', (req, res) => {
+ //signup page get
+    app.get('/reg', (req, res) => {
 
         res.render('signup');
     })
-
-    app.get(function (req, res, next) {
-
+//create signun api
+    app.use(function (req, res, next) {
+//header
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
         res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
@@ -27,11 +27,11 @@ module.exports = function (app) {
     app.post('/signup', async function (req, res) {
         console.log(JSON.stringify(req.body));
         try {
-
+            //insert value in boody
             const result = await knex("library.registration")
                 .insert({name: req.body.name, email: req.body.email, password: req.body.password, phone: req.body.phone, address: req.body.address})
                 .returning('*');
-
+            //respons send
             console.log(`result ${JSON.stringify(result)}`);
             res
                 .sendStatus(200)
